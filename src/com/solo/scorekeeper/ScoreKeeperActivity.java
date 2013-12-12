8055720a107b53210c7b46fa62af6da424533b75
Peter Cheng <petercheng00@gmail.com>
2013-12-11 22:35:13 -0800

    here is the first commit

---
+++ src/com/solo/scorekeeper/ScoreKeeperActivity.java
@@ -0,0 +1,66 @@
+package com.solo.scorekeeper;
+
+import java.util.ArrayList;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class ScoreKeeperActivity extends Activity {
+	ArrayList<TextView> player_scores;
+	ArrayList<Button> player_buttons;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_score_keeper);
+		
+		player_scores = new ArrayList<TextView>();
+		player_buttons = new ArrayList<Button>();
+		
+		player_scores.add((TextView) findViewById(R.id.player1_score));
+		player_scores.add((TextView) findViewById(R.id.player2_score));
+		player_scores.add((TextView) findViewById(R.id.player3_score));
+		
+		player_buttons.add((Button) findViewById(R.id.player1_btn));
+		player_buttons.add((Button) findViewById(R.id.player2_btn));
+		player_buttons.add((Button) findViewById(R.id.player3_btn));
+		
+		resetScores();
+		setupButtons();
+	}
+	
+	private void resetScores(){
+		for(TextView score : player_scores){
+			score.setText("0");
+		}
+	}
+
+	private void setupButtons(){
+		int counter = 0;
+		for(Button button: player_buttons){
+			TextView currentScore = player_scores.get(counter);
+			button.setOnClickListener(new ScoreButtonListener(currentScore));
+			counter +=1 ;
+		}
+	}
+	
+	protected class ScoreButtonListener implements OnClickListener{
+		TextView scoreView;
+		
+		public ScoreButtonListener(TextView scoreView){
+			this.scoreView = scoreView;
+		}
+		@Override
+		public void onClick(View v) {
+			int currentScore = Integer.parseInt((String) scoreView.getText());
+			int newScore = currentScore + 1; //user input
+			scoreView.setText(Integer.toString(newScore));
+		}
+		
+	}
+}
